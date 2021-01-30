4c7d747d001d7be45d9ccca93fca829396057d0c
tosh7 <zlia.6.lj.425@gmail.com>
2021-01-30 16:37:16 +0900

    Initial Commit

---
+++ IBPCompositionalLayout-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBPCompositionalLayout-Sample
+//
+//  Created by Satoshi Komatsu on 2021/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
